018890718c0104a453b1e71d33c095de5e932c6e
Bans.C <baiylnuygah@gmail.com>
2022-11-18 14:20:24 +0800

    Initialize project structure
    Initialize project structure

---
+++ src/PostBoy.Api/Startup.cs
@@ -0,0 +1,42 @@
+using System.Reflection;
+
+namespace PostBoy.Api;
+
+public class Startup
+{
+    private IConfiguration Configuration { get; }
+
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddMvc();
+        services.AddSwaggerGen(options => {
+            var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename),true);
+        });
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseCookiePolicy();
+        app.UseRouting();
+        app.UseAuthentication();
+        app.UseAuthorization();        
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
